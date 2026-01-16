c88b89e52ff22e42602a9b9bcfa60fed390419c7
Bartosz Cholewa <bbcholewa@gmail.com>
2026-01-16 17:28:01 +0100

    Lesson 12 - Player Weapon

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,6 +2,7 @@ class_name Player
 extends CharacterBody2D
 
 @onready var player_input_synchronizer_component: PlayerInputSynchronizerComponent = $PlayerInputSynchronizerComponent
+@onready var weapon_root: Node2D = $WeaponRoot
 
 var input_multiplayer_authority: int
 
@@ -9,10 +10,13 @@ func _ready() -> void:
 	# Set player authority as soon as node is ready
 	player_input_synchronizer_component.set_multiplayer_authority(input_multiplayer_authority)
 
-	# Only process this node from server, but not client
-	set_process(is_multiplayer_authority())
-
 
 func _process(_delta: float) -> void:
-	velocity = player_input_synchronizer_component.movement_vector * 100
-	move_and_slide()
+	# Rotate and point weapon at mouse position (aim_vector)
+	var aim_position: Vector2 = weapon_root.global_position + player_input_synchronizer_component.aim_vector
+	weapon_root.look_at(aim_position)
+
+	# Move players only from the server
+	if is_multiplayer_authority():
+		velocity = player_input_synchronizer_component.movement_vector * 100
+		move_and_slide()
