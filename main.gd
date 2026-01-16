b0bd7c6d2d8977e98bc544431810fcbf9ed8b267
Bartosz Cholewa <bbcholewa@gmail.com>
2026-01-16 21:52:45 +0100

    Lesson 19 - Arena

--- main.gd
+++ main.gd
@@ -3,6 +3,7 @@ extends Node
 const PLAYER_SCENE: PackedScene = preload("uid://cse7rqp64wxuc")
 const ENEMY_SCENE: PackedScene = preload("uid://c7u2ej7yguwy6")
 
+@onready var player_spawn_position: Marker2D = $PlayerSpawnPosition
 @onready var multiplayer_spawner: MultiplayerSpawner = $MultiplayerSpawner
 
 func _ready() -> void:
@@ -12,9 +13,8 @@ func _ready() -> void:
 
 	if is_multiplayer_authority():
 		var enemy_instance: Enemy = ENEMY_SCENE.instantiate()
-		enemy_instance.global_position = Vector2.ONE * 100
+		enemy_instance.global_position = Vector2.ONE * 200
 		add_child(enemy_instance)
-
 
 
 func _spawn_player(data: Dictionary) -> Player:
@@ -22,6 +22,7 @@ func _spawn_player(data: Dictionary) -> Player:
 	var peer_id: int = data["peer_id"]
 	player_instance.name = str(peer_id)
 	player_instance.input_multiplayer_authority = peer_id
+	player_instance.global_position = player_spawn_position.global_position
 	return player_instance
 
 @rpc("any_peer", "call_local", "reliable")
