bf30c7e1975fa2a75e8b917829a26681346a617a
Bartosz Cholewa <bbcholewa@gmail.com>
2026-01-16 22:46:41 +0100

    Lesson 21 - Basic Enemy AI

--- entities/enemy/enemy.gd
+++ entities/enemy/enemy.gd
@@ -2,18 +2,54 @@ class_name Enemy
 extends CharacterBody2D
 
 @onready var area_2d: Area2D = $Area2D
+@onready var target_acquisition_timer: Timer = $TargetAcquisitionTimer
+
 
 var current_health: int = 1
+var target_position: Vector2
 
 
 func _ready() -> void:
 	area_2d.area_entered.connect(_on_area_entered)
+	target_acquisition_timer.timeout.connect(_on_target_acqusition_timer_timeout)
+	if is_multiplayer_authority():
+		acquire_target()
+
+
+func _process(_delta: float) -> void:
+	if is_multiplayer_authority():
+		velocity = global_position.direction_to(target_position) * 40
+		move_and_slide()
+
 
 
 func handle_hit() -> void:
 	current_health -= 1
 	if current_health <= 0:
 		queue_free()
+
+func acquire_target() -> void:
+	var players: Array[Node] = get_tree().get_nodes_in_group("player")
+	var nearest_player: Player = null
+	var nearest_squared_distance: float
+
+	for player: Player in players:
+		if not nearest_player:
+			nearest_player = player
+			nearest_squared_distance = nearest_player.global_position\
+				.distance_squared_to(global_position)
+			continue
+
+		var player_squared_distance: float = player.global_position\
+			.distance_squared_to(global_position)
+		if player_squared_distance < nearest_squared_distance:
+			nearest_squared_distance = player_squared_distance
+			nearest_player = player
+
+	if nearest_player != null:
+		target_position = nearest_player.global_position
+
+
 
 
 func _on_area_entered(other_area: Area2D) -> void:
@@ -24,3 +60,7 @@ func _on_area_entered(other_area: Area2D) -> void:
 		var bullet: Bullet = other_area.owner
 		bullet.register_collision()
 		handle_hit()
+
+func _on_target_acqusition_timer_timeout() -> void:
+	if is_multiplayer_authority():
+		acquire_target()
