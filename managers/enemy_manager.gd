308f881993c8c01da0813f7985c1458e4f8fc06c
Bartosz Cholewa <bbcholewa@gmail.com>
2026-01-17 05:11:57 +0100

    Lesson 22 - Enemy Spawn Round

--- managers/enemy_manager.gd
+++ managers/enemy_manager.gd
@@ -1,14 +1,33 @@
 extends Node
 
 const ENEMY_SCENE: PackedScene = preload("uid://c7u2ej7yguwy6")
+const ROUND_BASE_TIME: int = 10
+const ROUND_GROWTH: int = 5
+const BASE_ENEMY_SPAWN_TIME: float = 2.0
+const ENEMY_SPAWN_TIME_GROWTH: float = -0.15
 
 @export var enemy_spawn_root: Node
 @export var spawn_rect: ReferenceRect
 
 @onready var spawn_interval_timer: Timer = $SpawnIntervalTimer
+@onready var round_timer: Timer = $RoundTimer
+
+var round_count: int = 0
 
 func _ready() -> void:
 	spawn_interval_timer.timeout.connect(_on_spawn_interval_timer_timeout)
+	round_timer.timeout.connect(_on_round_timer_timeout)
+	begin_round()
+
+
+func begin_round() -> void:
+	round_count += 1
+	round_timer.wait_time = ROUND_BASE_TIME + ((round_count - 1) * ROUND_GROWTH)
+	round_timer.start()
+
+	spawn_interval_timer.wait_time = BASE_ENEMY_SPAWN_TIME +\
+		((round_count - 1) * ENEMY_SPAWN_TIME_GROWTH)
+	spawn_interval_timer.start()
 
 
 func get_random_spawn_position() -> Vector2:
@@ -26,3 +45,9 @@ func spawn_enemy() -> void:
 func _on_spawn_interval_timer_timeout() -> void:
 	if is_multiplayer_authority():
 		spawn_enemy()
+		spawn_interval_timer.start()
+
+func _on_round_timer_timeout() -> void:
+	if is_multiplayer_authority():
+		spawn_interval_timer.stop()
+		print("round over")
