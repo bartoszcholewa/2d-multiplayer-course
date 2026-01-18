194c7208fd8d496a484bac8e4d00964ade819c55
Bartosz Cholewa <bbcholewa@gmail.com>
2026-01-18 19:33:22 +0100

    Lesson 29 - Rotation and Flipping player

--- entities/player/player.gd
+++ entities/player/player.gd
@@ -2,9 +2,10 @@ class_name Player
 extends CharacterBody2D
 
 @onready var player_input_synchronizer_component: PlayerInputSynchronizerComponent = $PlayerInputSynchronizerComponent
-@onready var weapon_root: Node2D = $WeaponRoot
+@onready var weapon_root: Node2D = $Visuals/WeaponRoot
 @onready var fire_rate_timer: Timer = $FireRateTimer
 @onready var health_component: HealthComponent = $HealthComponent
+@onready var visuals: Node2D = $Visuals
 
 const BULLET_SCENE: PackedScene = preload("uid://ci3xnymrb32hv")
 
@@ -18,9 +19,7 @@ func _ready() -> void:
 
 
 func _process(_delta: float) -> void:
-	# Rotate and point weapon at mouse position (aim_vector)
-	var aim_position: Vector2 = weapon_root.global_position + player_input_synchronizer_component.aim_vector
-	weapon_root.look_at(aim_position)
+	update_aim_position()
 
 	# Move players only from the server
 	if is_multiplayer_authority():
@@ -28,6 +27,16 @@ func _process(_delta: float) -> void:
 		move_and_slide()
 		if player_input_synchronizer_component.is_attack_pressed:
 			try_create_bullet()
+
+func update_aim_position() -> void:
+	# Rotate and point weapon at mouse position (aim_vector)
+	var aim_vector: Vector2 = player_input_synchronizer_component.aim_vector
+	var aim_position: Vector2 = weapon_root.global_position + aim_vector
+
+	# Flip sprite based on direction
+	visuals.scale = Vector2.ONE if aim_vector.x >= 0 else Vector2(-1, 1)
+
+	weapon_root.look_at(aim_position)
 
 
 func try_create_bullet() -> void:
