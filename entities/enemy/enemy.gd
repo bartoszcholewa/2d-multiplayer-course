f79174234730901b3f5792e7d0a3d096d36b270f
Bartosz Cholewa <bbcholewa@gmail.com>
2026-01-21 22:52:39 +0100

    Lesson 38 - State Machine and Enemy States

--- entities/enemy/enemy.gd
+++ entities/enemy/enemy.gd
@@ -6,40 +6,82 @@ extends CharacterBody2D
 @onready var visuals: Node2D = $Visuals
 
 var target_position: Vector2
-var is_spawning: bool
-
+var state_machine: CallableStateMachine = CallableStateMachine.new()
 
 func _ready() -> void:
+	# Spawning states
+	state_machine.add_states(
+		normal_state_spawn,
+		enter_state_spawn,
+		exit_state_spawn,
+	)
+
+	# Follow player state
+	state_machine.add_states(
+		normal_state_follow,
+		enter_state_follow,
+		exit_state_follow,
+	)
+
+	# Setting initial state as spawning
+	state_machine.set_initial_state(normal_state_spawn)
+
 	target_acquisition_timer.timeout.connect(_on_target_acqusition_timer_timeout)
-	play_spawn_animation()
 
 	if is_multiplayer_authority():
 		health_component.died.connect(_on_died)
-		acquire_target()
 
 
 func _process(_delta: float) -> void:
-	if is_multiplayer_authority() and not is_spawning:
-		velocity = global_position.direction_to(target_position) * 40
-		move_and_slide()
+	state_machine.update()
 
-	if not is_spawning:
-		flip()
 
-func flip() -> void:
-	visuals.scale = Vector2.ONE if target_position.x > global_position.x\
-		else Vector2(-1, 1)
+#region Enemy States
 
-func play_spawn_animation() -> void:
-	is_spawning = true
+#region Spawn State
+func enter_state_spawn() -> void:
 	var tween: Tween = create_tween()
 	tween.tween_property(visuals, "scale", Vector2.ONE, 0.4)\
 		.from(Vector2.ZERO)\
 		.set_ease(Tween.EASE_OUT)\
 		.set_trans(Tween.TRANS_BACK)
 	tween.finished.connect(func () -> void:
-		is_spawning = false
+		state_machine.change_state(normal_state_follow)
 	)
+
+func normal_state_spawn() -> void:
+	pass
+
+func exit_state_spawn() -> void:
+	pass
+#endregion
+
+#region Attack State
+func enter_state_follow() -> void:
+	if is_multiplayer_authority():
+		acquire_target()
+
+func normal_state_follow() -> void:
+	if is_multiplayer_authority():
+		velocity = global_position.direction_to(target_position) * 40
+		move_and_slide()
+	flip()
+
+
+func exit_state_follow() -> void:
+	pass
+#endregion
+
+#endregion
+
+
+
+
+
+func flip() -> void:
+	visuals.scale = Vector2.ONE if target_position.x > global_position.x\
+		else Vector2(-1, 1)
+
 
 func acquire_target() -> void:
 	var players: Array[Node] = get_tree().get_nodes_in_group("player")
---
+++ scripts/callable_state_machine.gd
@@ -0,0 +1,53 @@
+class_name CallableStateMachine
+
+var state_dictionary: Dictionary = {}
+var current_state: String
+
+
+func add_states(
+	normal_state_callable: Callable,
+	enter_state_callable: Callable,
+	exit_state_callable: Callable
+) -> void:
+	state_dictionary[normal_state_callable.get_method()] = {
+		"normal": normal_state_callable,
+		"enter": enter_state_callable,
+		"exit": exit_state_callable
+	}
+
+
+func set_initial_state(state_callable: Callable) -> void:
+	var state_name: StringName = state_callable.get_method()
+	if state_dictionary.has(state_name):
+		_set_state(state_name)
+	else:
+		push_warning("No state with name " + state_name)
+
+
+func update() -> void:
+	if current_state != null:
+		var normal_state: Callable = state_dictionary[current_state].normal
+		print("Calling ", normal_state.get_method())
+		normal_state.call()
+
+
+func change_state(state_callable: Callable) -> void:
+	var state_name: StringName = state_callable.get_method()
+	if state_dictionary.has(state_name):
+		_set_state.call_deferred(state_name)
+	else:
+		push_warning("No state with name " + state_name)
+
+
+func _set_state(state_name: String) -> void:
+	if current_state:
+		var exit_callable: Callable = state_dictionary[current_state].exit
+		if !exit_callable.is_null():
+			print("Calling ", exit_callable.get_method())
+			exit_callable.call()
+
+	current_state = state_name
+	var enter_callable: Callable = state_dictionary[current_state].enter
+	if !enter_callable.is_null():
+		print("Calling ", enter_callable.get_method())
+		enter_callable.call()
